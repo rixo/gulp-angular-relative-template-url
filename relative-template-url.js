cf7587c7e2802dcb020d2ee60bad233adc8617dd
Éric Ortega <eric.ortega@eoko.fr>
2015-10-29 17:28:19 +0100

    Support for ng-include element

--- relative-template-url.js
+++ relative-template-url.js
@@ -16,11 +16,16 @@ module.exports = {
 function relativeTemplateUrl(html, options) {
 
   // TODO
-  var search = html
+  var searches = html
     // regex for html
-    ? /((?:-|\b)template-url\b\s*=\s*['"])\.\/([^'"]+\.html)(['"])/
+    ? [
+      /((?:-|\b)template-url\b\s*=\s*['"])(\.{1,2}\/[^'"]+\.html)(['"])/g,
+      /(\bng-include\b\s*=\s*(?:'"|"'))(\.{1,2}\/[^'"]+\.html)('"|"')/g
+    ]
     // regex for js
-    : /\b(['"]?templateUrl['"]?\b\s*:\s*['"])\.\/([^'"]+)(['"])/;
+    : [
+      /\b(['"]?templateUrl['"]?\b\s*:\s*['"])(\.{1,2}\/[^'"]+)(['"])/g
+    ];
 
   return new Transform({
     objectMode: true,
@@ -41,11 +46,15 @@ function relativeTemplateUrl(html, options) {
         };
 
         if (file.isStream()) {
-          file.contents.pipe(rs(search, replacement));
+          searches.forEach(function(search) {
+            file.contents.pipe(rs(search, replacement));
+          });
         } else if (file.isBuffer()) {
-          file.contents = new Buffer(
-            String(file.contents).replace(search, replacement)
-          );
+          searches.forEach(function(search) {
+            file.contents = new Buffer(
+              String(file.contents).replace(search, replacement)
+            );
+          });
 
           //var contents = String(file.contents);
           //if (/templateUrl/.test(contents)) {
